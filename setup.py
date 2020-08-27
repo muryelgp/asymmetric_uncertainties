2a9c78f055574e7a328c676dd72bc78f506148ea
Muryel Guolo <muryel@astro.ufsc.br>
2020-08-27 03:24:00 -0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('license.txt') as f:
+    license = f.read()
+
+setup(
+    name='asymmetric_uncertainties',
+    version='1.0.0',
+    description='A package for dealing and propagating asymmetric uncertainties in measurements using Monte Carlo simulations.',
+    long_description=readme,
+    author='Muryel Guolo Pereira',
+    author_email='muryel@astro.ufsc.br',
+    url='https://github.com/muryelgp/asymmetric_uncertainties',
+    download_url = 'https://github.com/muryelgp/asymmetric_uncertainties/archive/master.zip',
+    license=license,
+    keywords = ['statistics', 'uncertanties', 'propagation'],
+    packages=['asymmetric_uncertainties'],
+    install_requires=[
+          'numpy',
+          'matplotlib',
+          'scipy',
+      ],
+)
